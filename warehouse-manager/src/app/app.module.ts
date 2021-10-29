609773d3dee86411cbde60898fc1a42dedb1b3d0
ultorhash <76662874+ultorhash@users.noreply.github.com>
2021-10-29 18:11:08 +0200

    update-packages and modules

--- warehouse-manager/src/app/app.module.ts
+++ warehouse-manager/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddEditModalComponent } from './components/add-edit-modal/add-edit-modal.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsTableComponent } from './components/products-table/products-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEditModalComponent,
+    ProductsTableComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
