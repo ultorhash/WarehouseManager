b933ed859505d4afcff9a5b938cc17da5032b047
ultorhash <76662874+ultorhash@users.noreply.github.com>
2021-10-31 15:02:29 +0100

    add dependecies

--- WarehouseAPI/Warehouse.App.Service/Data/Models/Employee.cs
+++ WarehouseAPI/Warehouse.App.Service/Data/Models/Employee.cs
@@ -1,13 +1,23 @@
-﻿namespace Warehouse.App.Service.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Warehouse.App.Service.Data.Models
 {
     public class Employee
     {
+        [Key]
+        [Required]
         public string Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Login { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public int RankdId { get; set; }
+        [Required]
         public string SubervisorId { get; set; }
     }
 }
---
+++ WarehouseAPI/Warehouse.App.Service/Data/Models/EmployeeDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.App.Service.Data.Models
+{
+    public class EmployeeDto
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
